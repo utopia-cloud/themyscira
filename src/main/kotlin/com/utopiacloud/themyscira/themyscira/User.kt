b8de4c22a3ffd74b95ef1cf7274cd8d910465c7c
Fumiya Goto <gotch@styz.io>
2019-02-17 17:11:07 +0900

    demo implementation of accessing data with MySQL https://spring.io/guides/gs/accessing-data-mysql/

---
+++ src/main/kotlin/com/utopiacloud/themyscira/themyscira/User.kt
@@ -0,0 +1,16 @@
+package com.utopiacloud.themyscira.themyscira
+
+import javax.persistence.*
+
+
+@Entity // This tells Hibernate to make a table out of this class
+@Table(name = "user")
+data class User(
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        var id: Long? = 0,
+
+        var name: String,
+
+        var email: String
+)
---
+++ src/main/kotlin/com/utopiacloud/themyscira/themyscira/UserRepository.kt
@@ -0,0 +1,9 @@
+package com.utopiacloud.themyscira.themyscira
+
+import org.springframework.data.repository.CrudRepository
+
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+interface UserRepository : CrudRepository<User, Int>
