66a4e1bf7d5a824d5bb6794bbaefdf1470fc2204
Fumiya Goto <gotch@styz.io>
2019-02-17 13:57:22 +0900

    first commit

---
+++ src/main/kotlin/com/utopiacloud/themyscira/themyscira/ThemysciraApplication.kt
@@ -0,0 +1,11 @@
+package com.utopiacloud.themyscira.themyscira
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThemysciraApplication
+
+fun main(args: Array<String>) {
+    runApplication<ThemysciraApplication>(*args)
+}
