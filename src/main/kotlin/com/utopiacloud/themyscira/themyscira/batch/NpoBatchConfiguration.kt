3f87d80a04ac32fd553210f8552b378f93674413
gotch <gotch@styz.io>
2019-02-19 11:10:01 +0900

    sample implementation of read csv.

--- src/main/kotlin/com/utopiacloud/themyscira/themyscira/batch/NpoBatchConfiguration.kt
+++ src/main/kotlin/com/utopiacloud/themyscira/themyscira/batch/NpoBatchConfiguration.kt
@@ -1,6 +1,9 @@
 package com.utopiacloud.themyscira.themyscira.batch
 
+import com.utopiacloud.themyscira.themyscira.ScheduledTasks
 import com.utopiacloud.themyscira.themyscira.application.NpoPortalService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
@@ -9,9 +12,15 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.launch.support.RunIdIncrementer
 import org.springframework.batch.core.step.tasklet.MethodInvokingTaskletAdapter
 import org.springframework.batch.core.step.tasklet.Tasklet
+import org.springframework.batch.item.ItemProcessor
+import org.springframework.batch.item.ItemWriter
+import org.springframework.batch.item.file.FlatFileItemReader
+import org.springframework.batch.item.file.LineMapper
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.FileSystemResource
 
 
 @Configuration
@@ -27,10 +36,11 @@ class NpoBatchConfiguration {
     lateinit var npoPortalService: NpoPortalService
 
     @Bean
-    fun downloadAndImportNpoDataJob(downloadZipStep: Step): Job {
+    fun downloadAndImportNpoDataJob(downloadZipStep: Step, readCsvStep: Step): Job {
         return jobBuilderFactory.get("downloadAndImportNpoDataJob")
                 .incrementer(RunIdIncrementer())
                 .start(downloadZipStep)
+                .start(readCsvStep)
                 .build()
     }
 
@@ -48,5 +58,70 @@ class NpoBatchConfiguration {
         tasklet.setTargetObject(npoPortalService)
         tasklet.setTargetMethod("downloadZipNpo")
         return tasklet
+    }
+
+    @Bean
+    fun readCsvStep(corporateCsvWriter: ConsoleItemWriter<String>): Step {
+        return stepBuilderFactory.get("readCsvStep")
+                .chunk<String, String>(10)
+                .reader(corporateCsvReader())
+                .processor(corporateItemProcessor())
+                .writer(corporateCsvWriter)
+                .build()
+    }
+
+    @Bean
+    fun corporateCsvReader(): FlatFileItemReader<String> {
+        return FlatFileItemReaderBuilder<String>()
+                .name("corporateCsvReader")
+                .resource(FileSystemResource("downloads/000_CorporateInputData_20190218.csv"))
+                .encoding("Shift_JIS")
+                .lineMapper(PassThroughLineMapper())
+//                .delimited()
+//                .names(arrayOf("firstName", "lastName"))
+//                .fieldSetMapper(object : BeanWrapperFieldSetMapper<String>() {
+//                    init {
+//                        setTargetType(String::class.java)
+//                    }
+//                })
+                .build()
+    }
+
+    @Bean
+    fun corporateItemProcessor(): CorporateItemProcessor {
+        return CorporateItemProcessor()
+    }
+
+    @Bean
+    fun corporateCsvWriter(): ConsoleItemWriter<String> {
+        return ConsoleItemWriter()
+    }
+}
+
+class PassThroughLineMapper : LineMapper<String> {
+    private val log: Logger = LoggerFactory.getLogger(ScheduledTasks::class.java)
+    override fun mapLine(line: String, lineNumber: Int): String {
+        log.info("PassThroughLineMapper")
+        log.info(line)
+        return line
+    }
+}
+
+class ConsoleItemWriter<T> : ItemWriter<T> {
+    private val log: Logger = LoggerFactory.getLogger(ScheduledTasks::class.java)
+
+    override fun write(items: MutableList<out T>) {
+        log.info("ConsoleItemWriter start")
+        items.forEach { item -> log.info(item.toString()) }
+        log.info("ConsoleItemWriter end")
+    }
+}
+
+class CorporateItemProcessor : ItemProcessor<String, String> {
+    private val log: Logger = LoggerFactory.getLogger(ScheduledTasks::class.java)
+    override fun process(item: String): String? {
+        log.info("CorporateItemProcessor")
+        log.info(item)
+        return item
     }
 }
